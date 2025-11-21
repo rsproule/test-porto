393495347ba7afbad3f4800062bdef5939a3570c
Ryan Sproule <rsproule23@gmail.com>
2025-11-21 10:51:27 -0500

    porto

--- lib/porto.ts
+++ lib/porto.ts
@@ -1,15 +1,9 @@
 import { Porto, Mode } from 'porto';
-import { Platform } from 'react-native';
 
 // Create Porto instance with React Native mode for mobile, dialog mode for web
 export const porto = Porto.create({
-  mode: Platform.select({
-    web: Mode.dialog(),
-    default: Mode.reactNative(),
-  }),
+  mode: Mode.reactNative(),
 });
 
 // Export Porto types for convenience
 export type { Porto } from 'porto';
-
-
